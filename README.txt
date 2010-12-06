ffc335c82f4735857471d9635a622eb92d5a45d6
Joshua Kerr <joshkerr@gmail.com>
2010-12-06 16:10:23 -0600

    Complete rewrite.   Added ability to scan through a give folder and process all MKV files.  Updated the readme file as well.  This script is working pretty well for me.  I still want to add a few features including some tag support.

--- README.txt
+++ README.txt
@@ -1,7 +1,20 @@
-README for mkvtom4v.sh shell script.
+PROJECT: MKV to M4V processing Script
+Author: Joshua Kerr
+License: BSD
+WEB: http://www.github.com/joshkerr/mkvtom4v
+CONTACT: joshkerr@gmail.com
 
 I am regularly downloading 720p H264 mkv files from the Internet and I wanted a quick way to get them into iTunes and use them on my iOS devices.  I found a few scripts and compiled them into this one script.  It identifies and extracts the video and audio.  It then encodes the audio into AAC and packages into a M4V container.
 
-Usage: mkvtom4v [filename]
+Usage: mkvtom4v.sh
 
-It will delete the mkv file if it successfully creates an m4v.  You might want to comment that out.  It also uses growl to identify you that the process is done.  Lastly, it loads the output into a subtitle app which let's me easily add chapter marks.
+The script will process all files in the current folder.  You can use the script to create a watch folder.  Let OSX automatically process movie files using folder script actions.  
+
+
+[2010-12-06: REVISION 2]
+
+[NEW] Now scans a given folder for all MKV's.  Processes everything.
+
+[FIXED] Would error out on ffmpeg when trying to convert certain AC3 files.
+
+[FIXED] Progress function on success would quit the script rather than go on to the next step.
--- mkvtom4v.sh
+++ mkvtom4v.sh
@@ -1,60 +1,216 @@
 #!/bin/bash
-
-find . -type f | grep .mkv$ | while read file
-
-do
-
-directory=`dirname "$file"`
-title=`basename "$file" .mkv`
-AC3=`mkvinfo "$file" | grep AC3` #check if it's AC3 audio or DTS
-AAC=`mkvinfo "$file" | grep AAC`
-order=`mkvinfo "$file" | grep "Track type" | sed 's/.*://' | head -n 1 | tr -d " "` #check if the video track is first or the audio track
-
-if [ "$order" = "video" ]; then
-  fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1` #store the fps of the video track
-  
-if [ -n "$AC3" ]; then
-   mkvextract tracks "$file" 1:"${title}".264 2:"${title}".ac3 
-   ffmpeg -i "${title}".ac3 -acodec libfaac -ab 576k "${title}".aac
-#  mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-#  faac -o "${title}".aac "${title}".wav
-  elif [ -n "$AAC" ]; then
-   mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-  else
-   mkvextract tracks "$file" 1:"${title}".264 2:"${title}".dts
-   ffmpeg -i "${title}".dts -acodec libfaac -ab 576k "${title}".aac
-  fi
-else
-  fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | tail -n 1`
-  if [ -n "$AC3" ]; then
-   mkvextract tracks "$file" 1:"${title}".ac3 2:"${title}".264
-   ffmpeg -i "${title}".ac3 -acodec libfaac -ab 576k "${title}".aac
-  # mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-  # faac -o "${title}".aac "${title}".wav
-  elif [ -n "$AAC" ]; then
-   mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-  else
-   mkvextract tracks "$file" 1:"${title}".dts 2:"${title}".264
-   ffmpeg -i "${title}".dts -acodec libfaac -ab 576k "${title}".aac
-  fi
-fi
-
-MP4Box -new "${directory}/${title}".m4v -add "${title}".264 -add "${title}".aac -fps $fps
-
-rm -f "$title".aac "$title".dts "$title".ac3 "$title".264 "${title}".wav
+# 
+#
+# SCRIPT: mkv to m4v
+# AUTHOR: Joshua Kerr
+# LICENSE: BSD
+# WEB: http://www.github.com/joshkerr/mkvtom4v
+# CONTACT: joshkerr@gmail.com
+#
+#
+# This script will convert all mkv files in the current directory by transcoding them into m4v files.  It assumes that you have x264 encoded video and either AC3 or DTS audio.
+#
+# Usage: mkvtom4v.sh
+#
+#
 
 
-if [ -f "${directory}/${title}".m4v ]; then
-	rm -f "$file"
-fi
+echo
+echo
 
-# set the tag on this to HD
-mp4tags -H 1 "${directory}/${title}".m4v
 
-#Growl
-growlnotify -m "Converted: ${title}.m4v" "Completed conversion of MKV file into M4V file." > /dev/null 2>&1
+VERSION="1.0"
 
-#Open iSubtitle to add chapter marks.  (Useful for iOS devices.)
-open -a /Applications/iSubtitle.app "${directory}/${title}".m4v
+# Set easy to use posix path's for OSX.  Remember the original setting so we can reset it at the end.
+ORIGINAL_IFS=$IFS
+IFS=$'\n'
+
+log="$PWD/log.$(date +%T-%F).txt"
+
+cecho() {
+	message=$1
+	echo -e "$message"	
+	echo -e "$message" >>"$log"	
+	tput sgr0;
+}
+
+ncecho () {
+	message=$1
+	echo -ne "$message"	
+	echo -ne "$message" >>"$log"	
+	tput sgr0;
+}
+
+sp="/-\|"
+spinny () {
+	echo -ne "\b${sp:i++%${#sp}:1}"
+	
+}
+
+progress () {
+	ncecho "  ";
+	while [ /bin/true ]; do
+		kill -0 $pid 2>/dev/null;
+		if [[ $? = "0" ]]; then
+			spinny
+			sleep 0.25
+		else
+			ncecho "\b\b";
+			wait $pid
+			retcode=$?
+			echo "$pid's retcode: $retcode" >> "$log"
+			if [[ $retcode = "0" ]]; then
+				cecho success
+			else
+				cecho failed
+				echo -e " [i] Showing the last 5 lines from the logfile ($log)...";
+				tail -n5 "$log"
+				exit 1;
+			fi
+			break 1;
+		fi
+	done
+}
+
+
+finish() {
+	cecho "\n [o] All done!\n";
+}
+
+cecho "\n [x] mkv script, v$VERSION, written by Joshua Kerr\n [x] Contact him at: http://www.joshkerr.com"
+
+#if [[ $# -lt 1 ]];
+#then
+	#cecho ' [i] Please supply a filename to remux to MP4.\n';
+	#exit 1;
+#fi
+
+for i in mplayer mencoder mkvinfo mkvextract MP4box ffmpeg; do
+	if hash -r "$i" >/dev/null 2>&1; then
+		ncecho;
+	else
+		cecho " [i] The $i command is not available, please install the required packages.\n";
+		DIE=1;
+	fi
+done
+
+if [[ $DIE ]]; then
+	cecho " [i] Needed programs weren't found, exiting...\n";
+	exit 1;
+fi		
+
+# Build the file name, directories and check for AC3 or DTS audio.
+
+for i in *.mkv; do
+
+	file=$i
+	
+	file_AC3="${i%.*}"".ac3"
+	file_AAC="${i%.*}"".aac"
+	file_264="${i%.*}"".264"
+	file_DTS="${i%.*}"".dts"
+
+	directory=`dirname "$file"`
+	title=`basename "$file" .mkv`
+	DEST="${1%.*}.m4v"
+	AC3=`mkvinfo "$file" | grep AC3` #check if it's AC3 audio or DTS
+	AAC=`mkvinfo "$file" | grep AAC`
+	order=`mkvinfo "$file" | grep "Track type" | sed 's/.*://' | head -n 1 | tr -d " "` #check if the video track is first or the audio track
+
+	cecho " [x] Source filename: $file\n [x] Destination filename: $DEST";
+
+	# Check for destination track before transcoding.
+	if [[ -f $DEST ]];
+	then
+		cecho ' [i] Destination filename already exists.\n';
+		exit 1;
+	fi
+
+	# Video is first in the order of tracks
+	if [ "$order" = "video" ]; then
+	  fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1` #store the fps of the video track
+ 
+		if [ -n "$AC3" ]; then
+			ncecho "\n [x] extracting video & audio tracks\n";
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".ac3 
+			pid=$!;progress $pid
+
+			ncecho "\n [x] Converting Audio into AAC\n";
+			ffmpeg -i "./$file_AC3" -acodec libfaac -ab 576k "./$file_AAC"
+			pid=$!;progress $pid
+	
+	#  mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
+	#  faac -o "${title}".aac "${title}".wav
+
+		elif [ -n "$AAC" ]; then
+			ncecho "\n [x] extracting video & audio tracks\n";	
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
+			pid=$!;progress $pid
+		else
+			ncecho "\n [x] extracting video & audio tracks\n";	
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".dts
+			pid=$!;progress $pid
+		
+			ncecho "\n [x] Converting Audio into AAC\n";
+	   		ffmpeg -i "./$file_DTS" -acodec libfaac -ab 576k "./$file_AAC".aac
+			pid=$!;progress $pid
+	  	fi
+	
+	# Audio is first in the track order
+	else
+	  	fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | tail -n 1`
+  	
+		if [ -n "$AC3" ]; then
+			ncecho "\n [x] extracting video & audio tracks\n";	
+	   		mkvextract tracks "$file" 1:"${title}".ac3 2:"${title}".264
+			pid=$!;progress $pid
+		
+			ncecho "\n [x] Converting Audio into AAC\n";
+	   		ffmpeg -i "./$file_AC3" -acodec libfaac -ab 576k "./$file_AAC"
+			pid=$!;progress $pid
+
+	  # mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
+	  # faac -o "${title}".aac "${title}".wav
+
+	  	elif [ -n "$AAC" ]; then
+			ncecho "\n [x] extracting video & audio tracks\n";	
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
+			pid=$!;progress $pid
+	  	else
+			ncecho "\n [x] extracting video & audio tracks\n";	
+	   		mkvextract tracks "$file" 1:"${title}".dts 2:"${title}".264
+			pid=$!;progress $pid
+
+			ncecho "\n [x] Converting Audio into AAC\n";
+	   		ffmpeg -i "./$file_DTS" -acodec libfaac -ab 576k "./$file_AAC"
+			pid=$!;progress $pid
+	  	fi
+	fi
+
+	ncecho "\n [x] Building m4v container.\n";
+	MP4Box -new "${directory}/${title}".m4v -add "${title}".264 -add "${title}".aac -fps $fps
+	pid=$!;progress $pid
+
+	#rm -f "$title".aac "$title".dts "$title".ac3 "$title".264 "${title}".wav
+
+
+	#if [ -f "${directory}/${title}".m4v ]; then
+		#rm -f "$file"
+	#fi
+
+	# set the tag on this to HD
+	mp4tags -H 1 "${directory}/${title}".m4v
+
+	finish;
 
 done
+
+#rm -f "$log"
+
+
+IFS=$ORIGINAL_IFS
+
+
+growlnotify -m "Converted: ${title}.m4v" "Completed conversion of MKV file into M4V file." > /dev/null 2>&1
+
+open -a /Applications/iFlicks.app "${directory}/${title}".m4v
