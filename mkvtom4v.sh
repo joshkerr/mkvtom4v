e772f20dc044e9f82833a6065307b56a546226e6
Joshua Kerr <joshkerr@gmail.com>
2010-12-07 00:08:43 -0500

    Cleaned things up a bit.  Removed buggy progress bar.

--- mkvtom4v.sh
+++ mkvtom4v.sh
@@ -14,12 +14,7 @@
 #
 #
 
-
-echo
-echo
-
-
-VERSION="1.0"
+VERSION="1.1"
 
 # Set easy to use posix path's for OSX.  Remember the original setting so we can reset it at the end.
 ORIGINAL_IFS=$IFS
@@ -36,54 +31,17 @@ cecho() {
 
 ncecho () {
 	message=$1
-	echo -ne "$message"	
+	echo -ne "$message\r"	
 	echo -ne "$message" >>"$log"	
 	tput sgr0;
 }
 
-sp="/-\|"
-spinny () {
-	echo -ne "\b${sp:i++%${#sp}:1}"
-	
-}
-
-progress () {
-	ncecho "  ";
-	while [ /bin/true ]; do
-		kill -0 $pid 2>/dev/null;
-		if [[ $? = "0" ]]; then
-			spinny
-			sleep 0.25
-		else
-			ncecho "\b\b";
-			wait $pid
-			retcode=$?
-			echo "$pid's retcode: $retcode" >> "$log"
-			if [[ $retcode = "0" ]]; then
-				cecho success
-			else
-				cecho failed
-				echo -e " [i] Showing the last 5 lines from the logfile ($log)...";
-				tail -n5 "$log"
-				exit 1;
-			fi
-			break 1;
-		fi
-	done
-}
-
-
 finish() {
-	cecho "\n [o] All done!\n";
+	cecho "\n [o] All done!";
 }
 
-cecho "\n [x] mkv script, v$VERSION, written by Joshua Kerr\n [x] Contact him at: http://www.joshkerr.com"
+cecho "\n [x] mkvtom4v script, v$VERSION, written by Joshua Kerr\n [x] Contact him at: http://www.joshkerr.com"
 
-#if [[ $# -lt 1 ]];
-#then
-	#cecho ' [i] Please supply a filename to remux to MP4.\n';
-	#exit 1;
-#fi
 
 for i in mplayer mencoder mkvinfo mkvextract MP4box ffmpeg; do
 	if hash -r "$i" >/dev/null 2>&1; then
@@ -103,8 +61,7 @@ fi
 
 for i in *.mkv; do
 
-	file=$i
-	
+	file=$i	
 	file_AC3="${i%.*}"".ac3"
 	file_AAC="${i%.*}"".aac"
 	file_264="${i%.*}"".264"
@@ -112,12 +69,12 @@ for i in *.mkv; do
 
 	directory=`dirname "$file"`
 	title=`basename "$file" .mkv`
-	DEST="${1%.*}.m4v"
+	DEST="${i%.*}.m4v"
 	AC3=`mkvinfo "$file" | grep AC3` #check if it's AC3 audio or DTS
 	AAC=`mkvinfo "$file" | grep AAC`
 	order=`mkvinfo "$file" | grep "Track type" | sed 's/.*://' | head -n 1 | tr -d " "` #check if the video track is first or the audio track
 
-	cecho " [x] Source filename: $file\n [x] Destination filename: $DEST";
+	cecho " [x] Source filename: $file\n [x] Destination filename: $DEST \n";
 
 	# Check for destination track before transcoding.
 	if [[ -f $DEST ]];
@@ -131,29 +88,14 @@ for i in *.mkv; do
 	  fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1` #store the fps of the video track
  
 		if [ -n "$AC3" ]; then
-			ncecho "\n [x] extracting video & audio tracks\n";
-	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".ac3 
-			pid=$!;progress $pid
-
-			ncecho "\n [x] Converting Audio into AAC\n";
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".ac3
 			ffmpeg -i "./$file_AC3" -acodec libfaac -ab 576k "./$file_AAC"
-			pid=$!;progress $pid
-	
-	#  mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-	#  faac -o "${title}".aac "${title}".wav
 
-		elif [ -n "$AAC" ]; then
-			ncecho "\n [x] extracting video & audio tracks\n";	
-	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-			pid=$!;progress $pid
+		elif [ -n "$AAC" ]; then	
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac 
 		else
-			ncecho "\n [x] extracting video & audio tracks\n";	
 	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".dts
-			pid=$!;progress $pid
-		
-			ncecho "\n [x] Converting Audio into AAC\n";
 	   		ffmpeg -i "./$file_DTS" -acodec libfaac -ab 576k "./$file_AAC".aac
-			pid=$!;progress $pid
 	  	fi
 	
 	# Audio is first in the track order
@@ -161,52 +103,35 @@ for i in *.mkv; do
 	  	fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | tail -n 1`
   	
 		if [ -n "$AC3" ]; then
-			ncecho "\n [x] extracting video & audio tracks\n";	
-	   		mkvextract tracks "$file" 1:"${title}".ac3 2:"${title}".264
-			pid=$!;progress $pid
-		
-			ncecho "\n [x] Converting Audio into AAC\n";
+	   		mkvextract tracks "$file" 1:"${title}".ac3 2:"${title}".264 
 	   		ffmpeg -i "./$file_AC3" -acodec libfaac -ab 576k "./$file_AAC"
-			pid=$!;progress $pid
-
-	  # mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-	  # faac -o "${title}".aac "${title}".wav
 
 	  	elif [ -n "$AAC" ]; then
-			ncecho "\n [x] extracting video & audio tracks\n";	
-	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-			pid=$!;progress $pid
+	   		mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac 
 	  	else
-			ncecho "\n [x] extracting video & audio tracks\n";	
 	   		mkvextract tracks "$file" 1:"${title}".dts 2:"${title}".264
-			pid=$!;progress $pid
-
-			ncecho "\n [x] Converting Audio into AAC\n";
 	   		ffmpeg -i "./$file_DTS" -acodec libfaac -ab 576k "./$file_AAC"
-			pid=$!;progress $pid
 	  	fi
 	fi
 
-	ncecho "\n [x] Building m4v container.\n";
-	MP4Box -new "${directory}/${title}".m4v -add "${title}".264 -add "${title}".aac -fps $fps
-	pid=$!;progress $pid
+	# Build container
+	MP4Box -new "${directory}/${title}".m4v -add "${title}".264 -add "${title}".aac -fps $fps 
 
-	#rm -f "$title".aac "$title".dts "$title".ac3 "$title".264 "${title}".wav
+	rm -f "$title".aac "$title".dts "$title".ac3 "$title".264 "${title}".wav 
 
-
-	#if [ -f "${directory}/${title}".m4v ]; then
-		#rm -f "$file"
-	#fi
+	if [ -f "${directory}/${title}".m4v ]; then
+		rm -f "$file"
+	fi
 
 	# set the tag on this to HD
+
 	mp4tags -H 1 "${directory}/${title}".m4v
 
 	finish;
 
 done
 
-#rm -f "$log"
-
+rm -f "$log"
 
 IFS=$ORIGINAL_IFS
 
